f409ae31032a0196e17028b936803738d4c9e496
Waldemar Quevedo <wally@nats.io>
2023-04-17 15:54:06 +0200

    wipiwp
    Signed-off-by: Waldemar Quevedo <wally@nats.io>

--- server/jetstream_test.go
+++ server/jetstream_test.go
@@ -8706,10 +8706,12 @@ func TestJetStreamMsgHeaders(t *testing.T) {
 			if mset.store == nil {
 				t.Fatal("Store is not ready")
 			}
+			state := mset.state()
+
+			t.Logf("Fast State: %+v", state)
 			result := mset.store.State()
 			t.Logf("Slow State: %+v", result)
 
-			state := mset.state()
 			if state.Msgs != 1 {
 				t.Fatalf("Expected 1 message, got %d", state.Msgs)
 			}
